9c90d47f38a8b5df81448c3936a262be93f369c9
Piyush Mishra <piyush11mishra@gmail.com>
2018-04-10 22:09:40 +0530

    Updated make

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
 if [ "$1" = "" ];then
     ext=""
 elif [ "$1" = "ts" ]; then
-    ext=$(date -Iseconds)
+    ext="_$(date -Iseconds)"
 else
     exit 1
 fi
 
 xelatex Resume_Piyush.tex 
 mv *.out *.log *.pdf *.aux output/
-mv output/Resume_Piyush.pdf output/Resume_Piyush_${ext}.pdf
+mv output/Resume_Piyush.pdf output/Resume_Piyush${ext}.pdf
